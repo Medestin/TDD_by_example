b56a40b49ddfdf13623660536c50bc5ae5fa13b3
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-03-28 14:34:04 +0100

    refactor equals() to compare currency, not class. new test passes

--- src/main/java/com/medestin/Money.java
+++ src/main/java/com/medestin/Money.java
@@ -21,7 +21,7 @@ public class Money {
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
     }
 
     @Override
