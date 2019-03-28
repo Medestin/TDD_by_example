b5572a0a00ffbb6dc917e4094a75f7c5c2449c1f
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-03-28 18:53:40 +0100

    feat: Bank gets Hashtable rates and implements addRate() method

--- src/main/java/com/medestin/Bank.java
+++ src/main/java/com/medestin/Bank.java
@@ -1,15 +1,20 @@
 package com.medestin;
 
-import java.util.Objects;
+import java.util.Hashtable;
 
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
+    void addRate(String from, String to, int rate){
+        rates.put(new Pair(from, to), rate);
+    }
 
     Money reduce(Expression source, String to){
         return source.reduce(this, to);
     }
 
     int rate(String from, String to){
-        return  (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        return (int) rates.get(new Pair(from, to));
     }
 
 
