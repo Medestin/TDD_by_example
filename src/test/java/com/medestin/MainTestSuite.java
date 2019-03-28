19849c44d81e4ff6931d02490f2fa67046fc4200
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-03-28 14:32:35 +0100

    feat: add new test to check equals between class and its parent

--- src/test/java/com/medestin/MainTestSuite.java
+++ src/test/java/com/medestin/MainTestSuite.java
@@ -36,4 +36,9 @@ public class MainTestSuite {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
+
+    @Test
+    public void testDifferentClassEquality(){
+        assertTrue(new Money(1, "CHF").equals(new Franc(1, "CHF")));
+    }
 }
