a2e6c6c3843ed7f73408d845eaa7554c24879d9a
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-03-28 13:49:22 +0100

    refactor <Money> to abstract and make a factory method for <Dollar>

--- src/main/java/com/medestin/Money.java
+++ src/main/java/com/medestin/Money.java
@@ -1,12 +1,18 @@
 package com.medestin;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount){
+        return new Dollar(amount);
     }
 }
